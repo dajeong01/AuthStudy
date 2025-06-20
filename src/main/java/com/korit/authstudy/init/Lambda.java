4e28a817d97f8aef2c19587abfccf74223bd8ffc
dajeong01 <wpekwjd@naver.com>
2025-06-20 10:43:20 +0900

    Lambda

--- src/main/java/com/korit/authstudy/init/Lambda.java
+++ src/main/java/com/korit/authstudy/init/Lambda.java
@@ -1,10 +1,56 @@
 package com.korit.authstudy.init;
 
+import com.korit.authstudy.domain.entity.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+class OptionalStudy<T> {
+    private final T present;
+
+    public void ifPresentOrElse(Consumer<T> action, Runnable runnable) {
+        if (present == null) {
+            action.accept(present);
+        } else {
+            runnable.run();
+        }
+    }
+}
 
 public class Lambda implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
+        User user = User.builder()
+                .id(100)
+                .username("test")
+                .password("1234")
+                .build();
 
+        // 람다 안 쓰는 코드
+        OptionalStudy<User> optionalStudy = new OptionalStudy<>(user);
+        Consumer<User> consumer = new Consumer<User>() {
+            @Override
+            public void accept(User user) {
+                System.out.println("user객체 찾음: " + user);
+            }
+        };
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("user객체 못 찾아서 여기서 다른 작업 할 거임.");
+            }
+        };
+        optionalStudy.ifPresentOrElse(consumer, runnable);
+
+        // 람다로 표현하면
+        Consumer<User> consumerLambda = (user1) -> {
+            System.out.println("user객체 찾음: " + user1);
+        };
+        Runnable runnableLambda = () -> {
+            System.out.println("user객체 못 찾아서 여기서 다른 작업 할 거임.");
+        };
+        optionalStudy.ifPresentOrElse(consumer, runnable);
     }
 }
